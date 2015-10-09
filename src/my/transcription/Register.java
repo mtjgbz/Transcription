b6c989c67a9d59db8c5178c3f4a87d4e03579c9d
mike <mike@mike-700Z3A-700Z4A-700Z5A-700Z5B>
2015-10-08 20:49:42 -0400

    Update register

--- src/my/transcription/Register.java
+++ src/my/transcription/Register.java
@@ -139,7 +139,8 @@ public class Register extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        dispose();
+        new Home().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
