52350afcc1f37dc24f84b01d8f42ffaab8c60ed0
Noah <Noah@dhcp-221-204.cc.gettysburg.edu>
2015-10-29 20:31:03 -0400

    passive buttons

--- src/my/transcription/Passive.java
+++ src/my/transcription/Passive.java
@@ -23,6 +23,8 @@ public class Passive extends javax.swing.JFrame {
     String user;
     long time1 = 0;
     long currentTime = 0;
+    int count = 0;
+    boolean pause = false;
     
     Enclitics enc = new Enclitics();
     Nasalizations nas = new Nasalizations();
@@ -77,7 +79,6 @@ public class Passive extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextPane5 = new javax.swing.JTextPane();
         jScrollPane3 = new javax.swing.JScrollPane();
@@ -88,6 +89,9 @@ public class Passive extends javax.swing.JFrame {
         jTextPane1 = new javax.swing.JTextPane();
         jScrollPane6 = new javax.swing.JScrollPane();
         jTextPane4 = new javax.swing.JTextPane();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 32767));
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -98,16 +102,6 @@ public class Passive extends javax.swing.JFrame {
         setMaximumSize(new java.awt.Dimension(970, 600));
         setMinimumSize(new java.awt.Dimension(970, 600));
         setResizable(false);
-
-        jButton1.setText("Play");
-        jButton1.setMaximumSize(new java.awt.Dimension(97, 29));
-        jButton1.setMinimumSize(new java.awt.Dimension(97, 29));
-        jButton1.setPreferredSize(new java.awt.Dimension(97, 29));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         jTextPane5.setMaximumSize(new java.awt.Dimension(550, 70));
         jTextPane5.setMinimumSize(new java.awt.Dimension(550, 70));
@@ -138,6 +132,29 @@ public class Passive extends javax.swing.JFrame {
         jTextPane4.setPreferredSize(new java.awt.Dimension(550, 70));
         jTextPane4.setSize(new java.awt.Dimension(550, 70));
         jScrollPane6.setViewportView(jTextPane4);
+
+        jButton1.setText("Play");
+        jButton1.setMaximumSize(new java.awt.Dimension(97, 29));
+        jButton1.setMinimumSize(new java.awt.Dimension(97, 29));
+        jButton1.setPreferredSize(new java.awt.Dimension(97, 29));
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton1MousePressed(evt);
+            }
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
+        jButton2.setText("Pause");
+        jButton2.setMaximumSize(new java.awt.Dimension(97, 29));
+        jButton2.setMinimumSize(new java.awt.Dimension(97, 29));
+        jButton2.setPreferredSize(new java.awt.Dimension(97, 29));
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton2MousePressed(evt);
+            }
+        });
 
         jMenu1.setText("Home");
         jMenu1.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -178,18 +195,23 @@ public class Passive extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(442, 442, 442))
             .addGroup(layout.createSequentialGroup()
-                .addGap(207, 207, 207)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(207, 207, 207)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(385, 385, 385)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(filler1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(0, 209, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -205,9 +227,16 @@ public class Passive extends javax.swing.JFrame {
                 .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(72, 72, 72))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(filler1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(66, 66, 66))
         );
 
         pack();
@@ -216,32 +245,6 @@ public class Passive extends javax.swing.JFrame {
 
 
     
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-//            if(!clip.isRunning() && currentTime == time1){
-//                clip.setMicrosecondPosition(time1);
-//                clip.start();
-//                jButton1.setText("Pause");
-//            }
-//            else if(!clip.isRunning() && currentTime != time1) {
-//                clip.setMicrosecondPosition(currentTime);
-//                clip.start();
-//                jButton1.setText("Pause"); 
-//            }
-//            else if(clip.isRunning()) { 
-//                currentTime = clip.getMicrosecondPosition();
-//                clip.stop();
-//                jButton1.setText("Play");
-//            }
-            long start = System.currentTimeMillis();
-            long end = 5000;
-            clip.setMicrosecondPosition(time1);
-            clip.start();
-            while(System.currentTimeMillis()-start<end){
-   
-            }
-            clip.stop();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void jMenu1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu1MouseClicked
        enc.dispose();
        nas.dispose();
@@ -273,6 +276,41 @@ public class Passive extends javax.swing.JFrame {
            tone.toFront();
         }
     }//GEN-LAST:event_jMenu4MouseClicked
+
+    private void jButton1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MousePressed
+
+    }//GEN-LAST:event_jButton1MousePressed
+
+    private void jButton2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MousePressed
+        pause = false;
+    }//GEN-LAST:event_jButton2MousePressed
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+               //            if(!clip.isRunning() && currentTime == time1){
+//                clip.setMicrosecondPosition(time1);
+//                clip.start();
+//                jButton1.setText("Pause");
+//            }
+//            else if(!clip.isRunning() && currentTime != time1) {
+//                clip.setMicrosecondPosition(currentTime);
+//                clip.start();
+//                jButton1.setText("Pause"); 
+//            }
+//            else if(clip.isRunning()) { 
+//                currentTime = clip.getMicrosecondPosition();
+//                clip.stop();
+//                jButton1.setText("Play");
+//            }
+            pause = true;
+            long start = System.currentTimeMillis();
+            long end = 5000;
+            clip.setMicrosecondPosition(time1);
+            clip.start();
+            while(System.currentTimeMillis()-start < end && pause){
+   
+            }
+            clip.stop();
+    }//GEN-LAST:event_jButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -310,7 +348,9 @@ public class Passive extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.Box.Filler filler1;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
