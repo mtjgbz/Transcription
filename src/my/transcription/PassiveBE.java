40b9404fcb5d99023e3e2179677d16aadde93c5a
Erica <Erica@Erica-PC>
2015-11-05 20:28:47 -0500

    Continued work on PassiveBE - findFiles method

--- src/my/transcription/PassiveBE.java
+++ src/my/transcription/PassiveBE.java
@@ -1,7 +1,9 @@
 package my.transcription;
 
+import java.io.*;
 import java.sql.*;
 import java.util.Random;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -17,6 +19,7 @@ public class PassiveBE {
     
     private Connection conn;
     private Statement stmt;
+    private ResultSet rs;
     private Random rand;
     
     /**
@@ -45,12 +48,46 @@ public class PassiveBE {
         try{
             conn.close();
             stmt.close();
+            rs.close();
         }catch(Exception e){
             e.printStackTrace();
         }
     }
 
     //Randomly select file from list of them in DB
+    public String findFile(int lesson, char sublesson){
+        try{
+            //pulling .txt file that contains lesson matches
+            String query = "SELECT(FileList) FROM LESSONS WHERE Lesson = " + lesson + 
+                    " AND Sublesson = '" + sublesson + "';";
+            rs = stmt.executeQuery(query);
+            String path = rs.getString("FileList");
+            
+            //pulling filename from lesson match .txt file
+            File file = new File(path);
+            LineNumberReader reader = new LineNumberReader(new FileReader(file));
+            
+            String line = reader.readLine();
+            while (line != null){
+                line = reader.readLine();
+            }
+            
+            int lineNum = reader.getLineNumber() - 1;
+            reader.setLineNumber(lineNum);
+            line = reader.readLine();
+            
+            int num = Integer.parseInt(line);
+            int random = rand.nextInt() % num;
+            
+            reader.setLineNumber(random);
+            path = reader.readLine();
+            return path;
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
     //Parse through document paths until random one reached
     //Create parser to go through file
     //Randomly select a line from that file
