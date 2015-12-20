bad5885b4d2dc33cf5b3665c9968dd998c9417e0
mike <mike@mike-700Z3A-700Z4A-700Z5A-700Z5B>
2015-12-20 16:27:27 -0500

    audioclip stuff

---
+++ src/my/transcription/AudioClip.java
@@ -0,0 +1,70 @@
+package my.transcription;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mike
+ */
+public class AudioClip {
+    File file;
+    long startPos;
+    long stopPos;
+    
+    public AudioClip(File file){
+       this.file = file;
+       stopPos = 0;
+    }
+    public AudioClip(File file, long startPos, long stopPos){
+        this.file=file;
+        this.startPos=startPos;
+        this.stopPos = stopPos;
+    }
+    
+    public void start(){
+        AudioInputStream audioIn = null;
+        try {
+            audioIn = AudioSystem.getAudioInputStream(file);
+            AudioFormat format = audioIn.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class,format);
+            SourceDataLine dataLine  = (SourceDataLine) AudioSystem.getLine(info);
+            dataLine.open(format);
+            float byteRate = format.getFrameSize()*format.getFrameRate();
+            byte[] bytesBuffer;
+            if(stopPos==0){
+                bytesBuffer = new byte[dataLine.getBufferSize()];
+            }else{
+                long difference = stopPos-startPos;
+                
+                float length = (float) (difference/1e6);
+                
+                int bytes = (int) (length*byteRate);
+                
+                bytesBuffer = new byte[bytes];
+            }   int off = (int) (byteRate*startPos/1e6);
+            int bytesRead = audioIn.read(bytesBuffer);
+            dataLine.write(bytesBuffer,off,bytesRead);
+            audioIn.close();
+            
+            dataLine.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            Logger.getLogger(AudioClip.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
